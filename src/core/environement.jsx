0204d6680cf7e5ec44d37a3df7364010d5e64c93
ahmed amine maarouf <ahmedaminemaarouf1@gmail.com>
2025-03-11 02:36:43 +0000

    ddd

--- src/core/environement.jsx
+++ src/core/environement.jsx
@@ -1,5 +1,5 @@
 const config = {
-    BACKEND_URL: process.env.REACT_APP_ENGINE_URL || "http://localhost:5000",
+    BACKEND_URL: process.env.REACT_APP_ENGINE_URL,
   };
   
   export default config; // Ensure you're exporting a named variable
