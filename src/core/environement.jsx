3d068b99e123a1d7f40805b502827e28cbc99f2c
ahmed amine maarouf <ahmedaminemaarouf1@gmail.com>
2025-03-11 11:55:09 +0000

    Added environment variable to Netlify

--- src/core/environement.jsx
+++ src/core/environement.jsx
@@ -1,5 +1,5 @@
 const config = {
-    BACKEND_URL: process.env.REACT_APP_ENGINE_URL,
+    BACKEND_URL: process.env.REACT_APP_ENGINE_URL, 
   };
   
   export default config; // Ensure you're exporting a named variable
